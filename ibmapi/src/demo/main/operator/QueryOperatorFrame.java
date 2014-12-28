10fcc78aa0e6c6c9be7627d2073cf36bf105edcb
wpexia <wpexia@gmail.com>
2014-12-28 22:15:00 +0800

    12 28

---
+++ ibmapi/src/demo/main/operator/QueryOperatorFrame.java
@@ -0,0 +1,40 @@
+package demo.main.operator;
+
+
+import gui.iBankGui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class QueryOperatorFrame extends iBankGui
+{
+	private static final long serialVersionUID = 503L;
+	private JTextField textOperatorId;
+
+
+	public QueryOperatorFrame(JFrame parent)
+	{
+		super(parent);
+
+		setTitle("iBank Query Operator Demo");
+
+		JLabel lbOperatorId = CreateLable("操作员编号");
+
+
+		textOperatorId = new JTextField();
+		textOperatorId.setColumns(15);
+		textOperatorId.addKeyListener(keyListener);
+
+		lbTitle.setText("查询操作员");
+		btnOK.setText("查询");
+
+
+		SetFont(textOperatorId);
+
+		AddInputComponent(lbOperatorId, 0, 0, 8, 1);
+		AddInputComponent(textOperatorId, 8, 0, GridBagConstraints.RELATIVE, 1);
+		AddInputComponent(btnOK, 0, 1, 8, 1);
+
+
+	}
+}
