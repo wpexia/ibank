cc40de926510aaa6b3106292efc81b5b15c0da6c
wpexia <wpexia@gmail.com>
2015-01-06 19:12:04 +0800

    debug

--- ibmapi/src/gui/iBankLogon.java
+++ ibmapi/src/gui/iBankLogon.java
@@ -6,6 +6,8 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+import ibankapi.SysConfig;
+
 public class iBankLogon extends JFrame
 {
 	private static final long serialVersionUID = 1L;
